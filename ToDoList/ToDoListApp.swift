57a47b08d082fbd1bb22408ca344e5f49063c575
shurup <magagaraev95@mail.ru>
2022-10-09 13:32:09 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Шарап Бамматов on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
